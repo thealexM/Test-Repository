de57582b3e99ebcf33f13cec55949efb07f7bfd1
thealexM <70339371+thealexM@users.noreply.github.com>
2020-09-14 18:03:14 -0400

    Add files via upload

---
+++ Morales_p1_decrypter.java
@@ -0,0 +1,54 @@
+package trask_p1;
+
+import java.util.Scanner;
+
+public class trask_p1_decrypter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scnr = new Scanner(System.in);
+
+		System.out.println("Welcome to the data decrypter!");
+
+		System.out.println("");
+			
+		System.out.println("Please enter four the digit integer you would like to decrypt:");
+
+		System.out.println("");
+			
+		int inVal = scnr.nextInt();
+		
+		int w = inVal;
+		int x = inVal;
+		int y = inVal;
+		int z = inVal;
+		
+		w = w / 1000;
+		x = (x / 100) % 10;
+		y = (y / 10) % 10;
+		z = z % 10;
+		
+		w = (w + 3);
+		x = (x + 3);
+		y = (y + 3);
+		z = (z + 3);
+
+		System.out.println("");
+			
+		System.out.println("Your data has been decrypted!");
+
+		System.out.println("");
+			
+		System.out.print(y);
+		System.out.print(z);
+		System.out.print(w);
+		System.out.println(x);
+
+		System.out.println("");
+			
+		System.out.println("Thank you for using the data decrypter!");
+		
+	}
+
+}
---
+++ Morales_p1_encrypter.java
@@ -0,0 +1,55 @@
+package Morales_p1;
+
+import java.util.Scanner;
+
+public class Morales_p1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scnr = new Scanner(System.in);
+
+		System.out.println("Welcome to the data encrypter!");
+
+		System.out.println("");
+			
+		System.out.println("Please enter four the digit integer you would like to encrypt:");
+
+		System.out.println("");
+			
+		int inVal = scnr.nextInt();
+		
+		int w = inVal;
+		int x = inVal;
+		int y = inVal;
+		int z = inVal;
+		
+		w = w / 1000;
+		x = (x / 100) % 10;
+		y = (y / 10) % 10;
+		z = z % 10;
+		
+		w = (w + 7) % 10;
+		x = (x + 7) % 10;
+		y = (y + 7) % 10;
+		z = (z + 7) % 10;
+
+		System.out.println("");
+			
+		System.out.println("Your data has been encrypted!");
+
+		System.out.println("");
+			
+		System.out.print(y);
+		System.out.print(z);
+		System.out.print(w);
+		System.out.println(x);
+
+		System.out.println("");
+			
+		System.out.println("Thank you for using the data encrypter!");
+			
+
+	}
+
+}
